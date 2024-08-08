ea15143152481ddcf8b216a7737890bd5826630d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-08 12:21:08 +0900

    chore: setup cross origin isolation

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,32 @@
 import { defineConfig } from "vite";
 import { vitePluginPreBundleNewUrl } from "@hiogawa/vite-plugin-pre-bundle-new-url";
 
+const crossOriginHeaders = {
+  'Cross-Origin-Opener-Policy': 'same-origin',
+  'Cross-Origin-Embedder-Policy': 'require-corp'
+}
+
 export default defineConfig({
   appType: "mpa",
   plugins: [
     vitePluginPreBundleNewUrl({
       debug: true
     }) as any,
+    {
+      name: "more-cross-origin",
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          for (const [k, v] of Object.entries(crossOriginHeaders)) {
+            res.setHeader(k, v);
+          }
+          next()
+        })
+      }
+    }
   ],
-  optimizeDeps: {
-    entries: ["./example/basic.html"]
-  }
+  server: {
+    headers: crossOriginHeaders
+  },
 })
 
 /*
